9e1ba76f4ea1a564e673bab6b1c9fc5a351a8e03
dramollei <dramolgry@gmail.com>
2020-09-23 12:18:21 +0200

    choosable scoring

--- tabliczkajs/tabliczka.js
+++ tabliczkajs/tabliczka.js
@@ -4,16 +4,29 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function getRightPercent(){
+    return isNaN(Math.round(questionsRight/questionsAnswered*100)) ? 0 : Math.round(questionsRight/questionsAnswered*100);
+}
 
 function koniecCzasu() {
     console.log("koniec!");
-    
+
     //ukryć wpisywanie
     document.getElementById("tabliczka").style.display = "none";
     //wyniki
-    document.getElementById("punkty").innerHTML=`${calculatePoints()} pkt.`
-    document.getElementById("ilenaile").innerHTML=`${questionsRight}/${questionsAnswered} (${Math.round(questionsRight/questionsAnswered*100)}%)`;
-    document.getElementById("reszta").innerHTML=`${(tgiven/questionsAnswered).toFixed(2)} sek./pytanie, maxPkt=${questionsAnswered*100}, %maxPkt=${Math.round(calculatePoints()/(questionsAnswered*100)*100)}%`
+    document.getElementById("punkty").innerHTML = `${calculatePoints()} pkt.`
+    document.getElementById("ilenaile").innerHTML = `${questionsRight}/${questionsAnswered} (${getRightPercent()}%)`;
+    switch (calculatePoints) {
+        case accScoring:
+            resztatxt = `${(tgiven/questionsAnswered).toFixed(2)} sek./pytanie, maxPkt=${questionsAnswered*100}, %maxPkt=${Math.round(calculatePoints()/(questionsAnswered*100)*100)}%`;
+            break;
+        case speedScoring:
+            resztatxt = `${(tgiven/questionsAnswered).toFixed(2)} sek./pytanie`;
+        default:
+            break;
+    }
+
+    document.getElementById("reszta").innerHTML = resztatxt;
     document.getElementById("wyniki").style.display = "initial";
 
 
@@ -33,7 +46,7 @@ function generateQuestion() {
     let liczba1 = getRandomIntInclusive(od1, do1);
     let liczba2 = getRandomIntInclusive(od2, do2);
     window.correctAnswer = liczba1 * liczba2;
-    document.getElementById("pytanie").innerHTML=`${liczba1} * ${liczba2} = `
+    document.getElementById("pytanie").innerHTML = `${liczba1} * ${liczba2} = `
 }
 
 function start() {
@@ -47,60 +60,75 @@ function start() {
     window.od2 = document.getElementById("od2").value;
     window.do2 = document.getElementById("do2").value;
     window.tgiven = document.getElementById("czas").value;
+    //punktowanie
+    sc = document.querySelector("input[name=scoring]:checked");
+    switch (sc.value) {
+        case "acc":
+            calculatePoints = accScoring;
+            break;
+        case "spd":
+            calculatePoints = speedScoring;
+            break;
+        default:
+            console.log('bru');
+            return;
+    }
     if (!(od1 && od2 && do1 && do2 && tgiven)) {
         return;
     }
     window.endTime = Date.now() + tgiven * 1000;
     window.counterTickID = setInterval(timeCounterTick, 50);
-
     //tabliczka widzialna parametry nie
     document.getElementById("tabliczka").style.display = "initial";
     document.getElementById("parametry").style.display = "none";
     //pierwsze pytanie
     generateQuestion();
     document.getElementById("odp").focus();
-
+    window.statUpdateID = setInterval(updateStats, 30);
 }
 
-function lqiRed(){
-    document.getElementById("lastqinfo").style.color="red";
+function lqiRed() {
+    document.getElementById("lastqinfo").style.color = "red";
 }
 
-function lqiGreen(){
-    document.getElementById("lastqinfo").style.color="green";
+function lqiGreen() {
+    document.getElementById("lastqinfo").style.color = "green";
 }
 
-function calculatePoints(){
-    return (questionsAnswered*(Math.pow(questionsRight/questionsAnswered, 2.1))*100).toFixed(0);
+function accScoring() {
+    return (questionsAnswered * (Math.pow(questionsRight / questionsAnswered, 2.1)) * 100).toFixed(0);
 }
 
-function sprawdz(){
-    let odpel=document.getElementById("odp");
-    let odp=odpel.value;
-    let lqi=document.getElementById("lastqinfo");
+function speedScoring() {
+    return (10000 / ((((tgiven * 1000 - (endTime - Date.now())) / 1000) / questionsAnswered)) / (1 + (questionsAnswered - questionsRight) * 0.1)).toFixed(0);
+}
+
+function sprawdz() {
+    let odpel = document.getElementById("odp");
+    let odp = odpel.value;
+    let lqi = document.getElementById("lastqinfo");
     questionsAnswered++;
-    if(odp==correctAnswer){
+    if (odp == correctAnswer) {
         questionsRight++;
         lqiGreen();
-        lqi.innerHTML="Dobrze!";
-    }else{
+        lqi.innerHTML = "Dobrze!";
+    } else {
         lqiRed();
-        lqi.innerHTML=`Źle... powinno być ${correctAnswer}`;
+        lqi.innerHTML = `Źle... powinno być ${correctAnswer}`;
     }
-    odpel.value="";
+    odpel.value = "";
     document.getElementById("odp").focus();
-    updateStats();
     generateQuestion();
 }
 
-function updateStats(){
-    st=document.getElementById("stats");
-    statStr=`${questionsRight}/${questionsAnswered} (${Math.round(questionsRight/questionsAnswered*100)}%), ${calculatePoints()} pkt.`
-    st.innerHTML=statStr;
+function updateStats() {
+    st = document.getElementById("stats");
+    statStr = `${questionsRight}/${questionsAnswered} (${getRightPercent()}%), ${calculatePoints()} pkt.`
+    st.innerHTML = statStr;
 }
 
-document.getElementById("odp").addEventListener("keydown", function(event){
-    if (event.keyCode === 13){
+document.getElementById("odp").addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) {
         document.getElementById("sprawdz").click();
     }
 });
